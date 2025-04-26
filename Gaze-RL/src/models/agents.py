b2185d6c3bd959cba687e01d9e2b24f9598607e5
Yusan1234 <yxa805terms@gmail.com>
2025-04-26 15:46:49 -0400

    modified train

--- Gaze-RL/src/models/agents.py
+++ Gaze-RL/src/models/agents.py
@@ -3,7 +3,9 @@
 from stable_baselines3 import PPO
 from .networks import CustomCNN
 from .gaze_predictor import GazePredictor
-from ..utils import compute_gaze_iou, compute_reward, compute_metrics
+from ..utils import compute_gaze_iou, GradCAM
+import torch
+
 class GazePPO(PPO):
     def __init__(self, config):
         policy_kwargs = {
@@ -14,14 +16,30 @@ class GazePPO(PPO):
         
         self.config = config
         
-        # Load gaze predictor from checkpoint
+        # Gaze predictor
         self.gaze_predictor = GazePredictor()
         self.gaze_predictor.load_state_dict(torch.load(config["gaze_predictor_path"]))
-        # gaze prediction 
-        # grad_cam
-        grad_cam_prediction = #TODO
-        gaze_prediction = self.gaze_predictor(obj)
-        gaze_iou = compute_gaze_iou(grad_cam_prediction, gaze_prediction) # TODO need to be checked for imgs size
-        reward = compute_reward(gaze_iou, config["gaze_iou_threshold"])
-        metrics = compute_metrics(reward, config["reward_threshold"])
+        self.gaze_predictor.eval()
+        
+        #TODO GradCAM extractor and you need to specify which layer you want
+        self.gradcam_extractor = GradCAM(
+            self.policy.features_extractor.backbone,
+            self.policy.features_extractor.backbone.layer4
+        )
+    def compute_gaze_reward(self, obs_tensor):
+        """
+        obs_tensor: torch.Tensor (1, C, H, W)
+        """
+        with torch.no_grad():
+            gradcam_map = self.gradcam_extractor.generate(obs_tensor)
+            gaze_map = self.gaze_predictor(obs_tensor)
+            #TODO need to aligh gradcam and gaze size
+            gaze_map = torch.nn.functional.interpolate(
+                gaze_map, size=gradcam_map.shape[-2:], mode='bilinear', align_corners=False
+            ).squeeze()
+
+        gaze_iou = compute_gaze_iou(gradcam_map, gaze_map)
+        r_gaze = self.config["lambda_gaze"] * gaze_iou
+
+        return r_gaze
         
--- Gaze-RL/src/utils.py
+++ Gaze-RL/src/utils.py
@@ -14,7 +14,47 @@ import matplotlib.pyplot as plt
 #     if gaze_heatmap:
 #         reward += 0.1 * iou(agent_attention, gaze_heatmap)
 #     return reward
+import torch
+import torch.nn.functional as F
 
+class GradCAM:
+    def __init__(self, model, target_layer):
+        self.model = model
+        self.target_layer = target_layer
+        self.gradients = None
+        self.activations = None
+        self._register_hooks()
+
+    def _register_hooks(self):
+        def forward_hook(module, input, output):
+            self.activations = output.detach()
+
+        def backward_hook(module, grad_input, grad_output):
+            self.gradients = grad_output[0].detach()
+
+        self.target_layer.register_forward_hook(forward_hook)
+        self.target_layer.register_full_backward_hook(backward_hook)
+
+    def generate(self, input_tensor, class_idx=None):
+        output = self.model(input_tensor)
+
+        if class_idx is None:
+            class_idx = output.argmax(dim=1)
+
+        self.model.zero_grad()
+        one_hot = torch.zeros_like(output)
+        one_hot[0, class_idx] = 1
+        output.backward(gradient=one_hot)
+
+        weights = self.gradients.mean(dim=(2, 3), keepdim=True)
+        cam = (weights * self.activations).sum(dim=1, keepdim=True)
+        cam = F.relu(cam)
+
+        cam = F.interpolate(cam, size=input_tensor.shape[2:], mode='bilinear', align_corners=False)
+        cam -= cam.min()
+        cam /= cam.max() + 1e-8
+
+        return cam.squeeze().cpu().numpy()
 
 def compute_reward(success, step_taken, gaze_iou, progress_delta,
                    λ=0.5, α=0.1):
